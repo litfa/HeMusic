b50b367d24b8f03abdba663090cc770a6a8c682f
Litfa <admin@litf.com.cn>
2022-05-07 20:16:36 +0800

    feat: 优化样式

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,26 @@ import Menu from './components/Menu/Menu.vue'
 </script>
 
 <template>
-  <Header></Header>
+  <div class="he-music">
+    <Header></Header>
     <Menu></Menu>
-  <router-view></router-view>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style>
+<style lang="less" scoped>
+.he-music {
+  display: grid;
+  grid-template-columns: 230px 1fr;
+  // 230
+  grid-template-rows: 100px 1fr 1fr;
+  // grid-column-gap: 0px;
+  // grid-row-gap: 0px;
+  .header {
+    grid-area: 1 / 1 / 2 / 3;
+  }
+  .menu {
+    grid-area: 2 / 1 / 3 / 2;
+  }
+}
 </style>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -15,7 +15,7 @@
 
 <style lang="less" scoped>
 .header {
-  width: 1120px;
+  width: 1200px;
   height: 44px;
   margin: 0 auto;
   display: flex;
@@ -24,7 +24,7 @@
   .logo {
     color: rgba(25, 31, 53, 1);
     letter-spacing: 2.4px;
-    font-size: 16px;
+    font-size: 20px;
     font-weight: bold;
     line-height: 40px;
     font-family: sans-serif;
