e8ca07f79ece410705533be815d48230df1ccdb9
Litfa <admin@litf.com.cn>
2022-05-07 16:30:13 +0800

    feat(components/Header): 导航栏

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="header">
+    <div>
+      <div class="logo">He Music</div>
+    </div>
+    <div></div>
+    <div></div>
+    <div class="logo"></div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.header {
+  width: 1120px;
+  height: 44px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .logo {
+    color: rgba(25, 31, 53, 1);
+    letter-spacing: 2.4px;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 40px;
+    font-family: sans-serif;
+  }
+}
+</style>
