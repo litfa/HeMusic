b073e9f20e79ddc00c3499535f2c79c362471f6b
Litfa <admin@litf.com.cn>
2022-05-08 15:15:53 +0800

    feat(components/Banner): banner

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -1,14 +1,34 @@
 <script lang="ts" setup>
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
+import { ElImage } from 'element-plus'
+import bannerApi from '@/apis/banner'
+import { ref } from 'vue'
 
+const banners = ref([])
+const show = ref(false)
+
+const getBanner = async () => {
+  const { data: res } = await bannerApi()
+  banners.value = res.banners
+  // 获取数据后刷新组件
+  show.value = true
+}
+getBanner()
 </script>
 
 <template>
-  <swiper :slides-per-view="3" :space-between="50" centered-slides loop>
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
+  <swiper
+    slides-per-view="auto"
+    centered-slides
+    :space-between="50"
+    loop
+    :looped-slides="1"
+    v-if="show"
+  >
+    <swiper-slide v-for="i in banners" :key="i.encodeId">
+      <el-image :src="i.imageUrl" />
+    </swiper-slide>
   </swiper>
 </template>
 
@@ -20,13 +40,12 @@ import 'swiper/css'
 
 .swiper-container {
   width: 100%;
-  height: 100%;
 }
 
 :deep(.swiper-wrapper) {
   .swiper-slide {
-    text-align: center;
-    font-size: 18px;
+    width: 380px !important;
+    height: 188px !important;
     background: #fff;
 
     /* Center slide text vertically */
@@ -43,7 +62,12 @@ import 'swiper/css'
     -webkit-align-items: center;
     align-items: center;
     transition: 300ms;
-    transform: scale(0.8);
+    transform: scale(0.7);
+    .el-image {
+      width: 100%;
+      height: 100%;
+      border-radius: 12px;
+    }
   }
   .swiper-slide-active,
   .swiper-slide-duplicate-active {
