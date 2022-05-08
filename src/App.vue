e62292091d7210abfe1a519889763dad4f13b049
Litfa <admin@litf.com.cn>
2022-05-08 16:24:34 +0800

    feat: 首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Header from './components/Header/Header.vue'
 import Menu from './components/Menu/Menu.vue'
+import Audio from './components/Audio/Audio.vue'
 </script>
 
 <template>
@@ -8,15 +9,17 @@ import Menu from './components/Menu/Menu.vue'
     <Header></Header>
     <Menu></Menu>
     <router-view></router-view>
+    <Audio></Audio>
   </div>
 </template>
 
 <style lang="less" scoped>
 .he-music {
+  height: 100vh;
   display: grid;
   grid-template-columns: 230px 1fr;
   // 230
-  grid-template-rows: 100px 1fr 1fr;
+  grid-template-rows: 60px 1fr 80px;
   // grid-column-gap: 0px;
   // grid-row-gap: 0px;
   .header {
@@ -24,6 +27,9 @@ import Menu from './components/Menu/Menu.vue'
   }
   .menu {
     grid-area: 2 / 1 / 3 / 2;
+  }
+  .audio {
+    grid-area: 3 / 1 / 4 / 3;
   }
 }
 </style>
