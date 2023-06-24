dcc64e02278952de37f1323980008bfc7ba6bd6d
ltfei <ltfei@ltfei.cn>
2023-06-24 11:38:32 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: resolve(__dirname, './dist/'),
+  base: resolve(__dirname, './'),
   plugins: [vue()],
   resolve: {
     alias: {
